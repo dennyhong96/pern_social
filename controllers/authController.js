2441d70421f244449fa82134f4d92c7b5db868b4
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-07-10 21:36:53 -0700

    Add register user route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const pool = require("../postgres");
+const bcrypt = require("bcryptjs");
 
 const registerValidator = require("../utils/validators/register");
 
@@ -12,12 +13,19 @@ exports.register = async (req, res, next) => {
 
     // Create user
     const { handle, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
     const user = await pool.query(
       "INSERT INTO users(handle, email, password) VALUES($1, $2, $3) RETURNING * ;",
-      [handle, email, password]
+      [handle, email, hashedPassword]
     );
-    console.log(user.rows);
+    res.status(201).json({ user: user.rows[0] });
   } catch (error) {
     console.error(error);
+    if (error.code === "23505") {
+      // Handle duplicate credentials
+      return res.status(400).json({ error: "Credentials already in use" });
+    } else {
+      return res.status(500).json({ error: "Something went wrong" });
+    }
   }
 };
