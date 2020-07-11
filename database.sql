a4c9b7ad86b3d746ddfc304913b5b3c57a9575d6
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-07-10 19:58:51 -0700

    Test out tables

--- database.sql
+++ database.sql
@@ -7,7 +7,7 @@ CREATE TABLE users(
 
 CREATE TABLE profiles(
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id uuid NOT NULL REFERENCES users(id),
+    user_id uuid NOT NULL REFERENCES users(id) UNIQUE,
     bio VARCHAR(255),
     photo VARCHAR(255) DEFAULT 'default.jpeg'
 );
@@ -30,3 +30,46 @@ CREATE TABLE likes(
     post_id uuid NOT NULL REFERENCES posts(id),
     user_id uuid NOT NULL REFERENCES users(id)
 );
+
+CREATE UNIQUE INDEX user_like_post ON likes (post_id, user_id);
+
+-- QUERIES
+-- CREATE A USER
+INSERT INTO
+    users(handle, email, password)
+VALUES
+    ('hhy666', 'hong@gmail.com', '12341234');
+
+-- CREATE A PROFILE
+INSERT INTO
+    profiles(user_id)
+VALUES
+    ('10a91530-1cda-49f6-9cd2-ebac1a3f2059');
+
+-- CREATE A POST
+INSERT INTO
+    posts(user_id, content)
+VALUES
+    (
+        '10a91530-1cda-49f6-9cd2-ebac1a3f2059',
+        'This is a test post.'
+    );
+
+-- CREATE A COMMENT
+INSERT INTO
+    comments(post_id, user_id, content)
+VALUES
+    (
+        '6ef72082-0c23-495a-a3da-6cfbc0b82bca',
+        '10a91530-1cda-49f6-9cd2-ebac1a3f2059',
+        'This is a test comment'
+    );
+
+-- LIKE A POST
+INSERT INTO
+    likes(post_id, user_id)
+VALUES
+    (
+        '6ef72082-0c23-495a-a3da-6cfbc0b82bca',
+        '10a91530-1cda-49f6-9cd2-ebac1a3f2059'
+    );
